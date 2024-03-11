1431d3958657b8bdef6967f0d2fd385ddd5de199
Pleasedierich <139540093+Pleasedierich@users.noreply.github.com>
2024-03-11 22:30:16 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-const FirstName = 'Kirill'
-console.log('Hello, my name is', FirstName)
+const FirstName = 'Kirill ';
+let LastName = 'Zhivoy';
+LastName = 'Kushnarev';
+const FullName = FirstName + LastName;
+const MyYears = '27 years old';
+const MyCity = 'Rostov-on-Don';
+let CourseName = 'Webflow Basic';
+CourseName = 'Webflow Pro';
 
-let LastName = 'Kushnarev'
-LastName = 'Zhivoi'
-console.log('And last name is', LastName)
+console.log('Hello, mentor. My name is', FullName);
+console.log('I am from', MyCity);
+console.log('I am', MyYears);
+console.log('My hobby is tudying the', CourseName);
