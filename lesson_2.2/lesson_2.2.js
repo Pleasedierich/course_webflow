1626e2046beca19ccebef49d18206499fe416a4a
Pleasedierich <139540093+Pleasedierich@users.noreply.github.com>
2024-03-12 18:26:35 +0300

    lesson_2.2

---
+++ lesson_2.2/lesson_2.2.js
@@ -0,0 +1,6 @@
+const titleProject = 'Оценка стоимости';
+const screensValue = 'шаблонные, с уникальным дизайном, с анимациями';
+const screenPrice = 5000;
+const percentage = 20;
+const fullPrice = 180000;
+const responsive = true;
